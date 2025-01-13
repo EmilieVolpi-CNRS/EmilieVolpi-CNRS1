07a9f838103d6b804fea8dfe09a731650f11c8bf
EmilieVolpi-CNRS <78140465+EmilieVolpi-CNRS@users.noreply.github.com>
2025-01-13 19:17:38 +0100

    Rename 10-cleaning_political_nuance.sql to 10-cleaning_political_nuance_2.2.2.sql

--- 10-cleaning_political_nuance.sql
+++
@@ -1,1 +0,0 @@
-update "BREF"."PoliticalNuance" set "PoliticalNuanceName" = TRIM("PoliticalNuanceName");
---
+++ 10-cleaning_political_nuance_2.2.2.sql
@@ -0,0 +1,1 @@
+update "BREF"."PoliticalNuance" set "PoliticalNuanceName" = TRIM("PoliticalNuanceName");
